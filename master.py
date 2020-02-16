4b1d1eb1ff04faff6a99cdddd84f8613d1aab41d
Teimuraz <Teimuraz@sakartvelo.net>
2020-02-16 13:01:42 +0400

    Understanding "little steps of improvement"

--- master.py
+++ master.py
@@ -1,9 +1,12 @@
 """
 System launcher
 """
+from sys_car import RootCar
 
 
 def main():
+    my_car = RootCar("Audi", "A6", "X1", "2008")
+    print(my_car)
     pass
 
 
--- modules/sys_car.py
+++ modules/sys_car.py
@@ -4,4 +4,30 @@ Car Class
 
 
 class RootCar:
-    pass
+    """
+    this is a primary car class
+    """
+
+    def __init__(self, xmaker, xmodel, xsubmodel, x_prod_year):
+        """
+        Root car class
+        :param xmaker:
+        Defines car maker
+        :param xmodel:
+        Defines car model
+        :param xsubmodel:
+        Defines car submodel as manufacturer describes
+        :param x_prod_year:
+        Defines car production year
+        """
+
+        self._maker = xmaker
+        self._model = xmodel
+        self._submodel = xsubmodel
+        self._year = x_prod_year
+
+    # ToDo research core characteristics
+
+    def __str__(self):
+        self.x_repr_str = f'This is an {self._maker} {self._model}, submodel {self._submodel} produced in {self._year}'
+        return self.x_repr_str
